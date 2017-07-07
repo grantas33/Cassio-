9a9184fe09a5a9ec94063aeb11ce9176d6a43ac3
Grantas Gadliauskas <gragad@ktu.lt>
2017-07-07 23:22:26 +0300

    Updated "Choose food" to work

--- app/src/main/java/com/example/grantas/cassio/ChooseFoodGrid.java
+++ app/src/main/java/com/example/grantas/cassio/ChooseFoodGrid.java
@@ -1,13 +1,13 @@
 package com.example.grantas.cassio;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
+
+import com.example.grantas.cassio.Tools.ImageAdapter;
 
 /**
  * Created by Grantas on 2017-07-07.
--- app/src/main/java/com/example/grantas/cassio/Tools/FragmentAdapter.java
+++ app/src/main/java/com/example/grantas/cassio/Tools/FragmentAdapter.java
@@ -1,12 +1,15 @@
-package com.example.grantas.cassio;
+package com.example.grantas.cassio.Tools;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.example.grantas.cassio.ChooseFoodGrid;
+
 
 /**
  * Created by Grantas on 2017-07-06.
+ * Klase, isdestanti fragmentus "Choose food"
  */
 
 public class FragmentAdapter extends FragmentPagerAdapter
